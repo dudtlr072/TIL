b03933cf3b62c00ab1a0b48f7849b2258c0921db
youngsik87.yoon <youngsik87.yoon@lge.com>
2018-01-12 12:01:30 +0900

    Update return app

--- etc/ReturnApp/app/src/main/java/com/yys/returnapp/DialogActivity.java
+++ etc/ReturnApp/app/src/main/java/com/yys/returnapp/DialogActivity.java
@@ -1,8 +1,10 @@
 package com.yys.returnapp;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,8 +12,9 @@ import android.widget.TextView;
  * Created by YYS on 2018-01-11.
  */
 
-public class DialogActivity extends Activity {
+public class DialogActivity extends AppCompatActivity {
 
+//    private Toolbar mToolbar;
     private TextView mMessageView;
     private Button mOK;
 
@@ -23,12 +26,22 @@ public class DialogActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog);
 
+//        mToolbar = (Toolbar)findViewById(R.id.my_toolbar);
+//        setSupportActionBar(mToolbar);
+
         mMessageView = (TextView)findViewById(R.id.message);
         mOK = (Button)findViewById(R.id.ok);
 
         mTitle = getIntent().getStringExtra("title");
         mMessage = getIntent().getStringExtra("message");
 
-
+//        getSupportActionBar().setTitle(mTitle);
+        mMessageView.setText(mMessage);
+        mOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- etc/ReturnApp/app/src/main/java/com/yys/returnapp/MyFirebaseMessagingService.java
+++ etc/ReturnApp/app/src/main/java/com/yys/returnapp/MyFirebaseMessagingService.java
@@ -1,20 +1,17 @@
 package com.yys.returnapp;
 
-import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Handler;
-import android.os.Looper;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-import com.firebase.jobdispatcher.FirebaseJobDispatcher;
-import com.firebase.jobdispatcher.GooglePlayDriver;
-import com.firebase.jobdispatcher.Job;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -32,8 +29,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData().get("message"));
-            handleNow();
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            String title = remoteMessage.getData().get("title");
+            String message = remoteMessage.getData().get("msg");
+            handleNow(title, message);
         }
     }
 
@@ -49,33 +48,44 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 //        // [END dispatch_job]
 //    }
 
-    private void handleNow() {
+    private void handleNow(String title, String message) {
         Log.d(TAG, "Short lived task is done.");
+        sendNotification("시료 반납");
+
         Intent intent = new Intent(this, DialogActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("title", title);
+        intent.putExtra("message", message);
         startActivity(intent);
     }
 
     private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
+//        Intent intent = new Intent(this, MainActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+//                PendingIntent.FLAG_ONE_SHOT);
 
         String channelId = "channel_id";
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
                         .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("FCM Message")
+                        .setContentTitle("Message")
                         .setContentText(messageBody)
                         .setAutoCancel(true)
                         .setSound(defaultSoundUri)
-                        .setContentIntent(pendingIntent);
+                        .setPriority(Notification.PRIORITY_HIGH)
+                        .setVibrate(new long[]{1, 1000});
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
