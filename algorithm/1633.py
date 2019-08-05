6200aaed4805e16f0ef062a70910100ec1e6665a
dudtlr072 <dudtlr072@gmail.com>
2019-08-06 01:10:16 +0900

    [algorithm] 1633 #2

--- algorithm/1633.py
+++ algorithm/1633.py
@@ -1,3 +1,4 @@
+TotalNum = 15
 whiteList = []
 blackList = []
 whiteCount = 0
@@ -23,18 +24,26 @@ while True:
 whiteList.sort(key=lambda white: white[0], reverse=True)
 blackList.sort(key=lambda black: black[1], reverse=True)
 
-for whiteIndex in range(0, 30):
+for whiteIndex in range(0, len(whiteList)):
+    print(whiteList[whiteIndex], " / ", blackList[whiteIndex])
+
     blackIndex = blackList.index(whiteList[whiteIndex])
-    if whiteIndex > blackIndex:
+    if whiteIndex < blackIndex and whiteCount < TotalNum:
         whiteTotalPower += whiteList[whiteIndex][0]
         whiteCount += 1
-    if whiteIndex < blackIndex:
+        print("white count:", whiteCount, " / value:", whiteList[whiteIndex][0], " / white total:", whiteTotalPower)
+    elif whiteIndex > blackIndex and blackCount < TotalNum:
         blackTotalPower += blackList[blackIndex][1]
         blackCount += 1
+        print("black count:", blackCount, " / value:", blackList[blackIndex][1], " / black total:", blackTotalPower)
     else:
-        if whiteCount < blackCount:
+        if whiteCount < blackCount and whiteCount < TotalNum:
             whiteTotalPower += whiteList[whiteIndex][0]
-        else:
+            whiteCount += 1
+            print("white count:", whiteCount, " / value:", whiteList[whiteIndex][0], " / white total:", whiteTotalPower)
+        elif whiteCount > blackCount and blackCount < TotalNum:
             blackTotalPower += blackList[whiteIndex][1]
+            blackCount += 1
+            print("black count:", blackCount, " / value:", blackList[blackIndex][1], " / black total:", blackTotalPower)
 
 print(whiteTotalPower + blackTotalPower)
